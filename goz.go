38f47af0c54a33ea022bf8798773d73426609f69
艾逗笔 <mikemintang@126.com>
2021-02-08 14:54:20 +0800

    add direct method

--- goz.go
+++ goz.go
@@ -10,3 +10,33 @@ func NewClient(opts ...Options) *Request {
 
 	return req
 }
+
+// Get send get request
+func Get(uri string, opts ...Options) (*Response, error) {
+	r := NewClient()
+	return r.Request("GET", uri, opts...)
+}
+
+// Post send post request
+func Post(uri string, opts ...Options) (*Response, error) {
+	r := NewClient()
+	return r.Request("POST", uri, opts...)
+}
+
+// Put send put request
+func Put(uri string, opts ...Options) (*Response, error) {
+	r := NewClient()
+	return r.Request("PUT", uri, opts...)
+}
+
+// Patch send patch request
+func Patch(uri string, opts ...Options) (*Response, error) {
+	r := NewClient()
+	return r.Request("PATCH", uri, opts...)
+}
+
+// Delete send delete request
+func Delete(uri string, opts ...Options) (*Response, error) {
+	r := NewClient()
+	return r.Request("DELETE", uri, opts...)
+}
